d66c1e210f449e0fc4b892caab4e3b217f4d25d4
claytantor <claytantor@cdd732f4-07f7-4c14-856a-aa9830f7e343>
2010-11-21 04:43:04 +0000

    small fixes for about and claim page
    git-svn-id: http://corp.ratecred.com/svn/sightly-ratecred-service/trunk@1724 cdd732f4-07f7-4c14-856a-aa9830f7e343

--- etl/080_20101114/updates01.sql
+++ etl/080_20101114/updates01.sql
@@ -2,9 +2,13 @@ INSERT INTO user (Host,User,Password) VALUES('10.210.37.193','ratecred',PASSWORD
 FLUSH PRIVILEGES;
 INSERT INTO user (Host,User,Password) VALUES('10.254.98.161','ratecred',PASSWORD('Rammy56Tally'));
 FLUSH PRIVILEGES;
+INSERT INTO user (Host,User,Password) VALUES('10.192.67.175','ratecred',PASSWORD('Rammy56Tally'));
+FLUSH PRIVILEGES;
 GRANT ALL ON ratecred_080_etl.* TO 'ratecred'@'10.210.37.193';
 FLUSH PRIVILEGES;
 GRANT ALL ON ratecred_080_etl.* TO 'ratecred'@'10.254.98.161';
+FLUSH PRIVILEGES;
+GRANT ALL ON ratecred_080_etl.* TO 'ratecred'@'10.192.67.175';
 FLUSH PRIVILEGES;
 
 
