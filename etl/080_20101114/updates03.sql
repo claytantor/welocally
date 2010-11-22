7fc612c8ce041102543042340b38b0556fb65044
claytantor <claytantor@cdd732f4-07f7-4c14-856a-aa9830f7e343>
2010-11-22 05:36:01 +0000

    etl for upgrade
    git-svn-id: http://corp.ratecred.com/svn/sightly-ratecred-service/trunk@1730 cdd732f4-07f7-4c14-856a-aa9830f7e343

---
+++ etl/080_20101114/updates03.sql
@@ -0,0 +1,4 @@
+update place set email="claytantor@gmail.com", business_services='true' where twitter_id='d5c6b0353b25af86';
+update place set email="marcoabartolozzi@yahoo.com", business_services='true' where twitter_id='d5c6b0353b25af86';
+
+
