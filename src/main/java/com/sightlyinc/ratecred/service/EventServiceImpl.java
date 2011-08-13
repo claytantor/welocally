384b33826d23c2d88de39ea609e4a71e706e4938
sam <sam@cdd732f4-07f7-4c14-856a-aa9830f7e343>
2011-08-13 01:59:02 +0000

    add setter for dao so i can inject a mock in a unit test, remove unused dependenceis
    git-svn-id: http://corp.ratecred.com/svn/sightly-ratecred-service/trunk@2071 cdd732f4-07f7-4c14-856a-aa9830f7e343

--- src/main/java/com/sightlyinc/ratecred/service/EventServiceImpl.java
+++ src/main/java/com/sightlyinc/ratecred/service/EventServiceImpl.java
@@ -1,17 +1,11 @@
 package com.sightlyinc.ratecred.service;
 
-import java.util.Calendar;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.noi.utility.spring.service.BLServiceException;
 import com.sightlyinc.ratecred.dao.BaseDao;
 import com.sightlyinc.ratecred.dao.EventDao;
 import com.sightlyinc.ratecred.model.Event;
-import com.sightlyinc.ratecred.model.Place;
-import com.sightlyinc.ratecred.model.Publisher;
-import com.simplegeo.client.types.Feature;
 
 /**
  * Class javadoc comment here...
@@ -43,6 +37,9 @@ public class EventServiceImpl extends AbstractTransactionalService<Event> implem
     public Event findByUrl(String url) {
         return eventDao.findByUrl(url);
     }
-    
-	
+
+
+    public void setEventDao(EventDao eventDao) {
+        this.eventDao = eventDao;
+    }
 }
