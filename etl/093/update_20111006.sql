7870048a861d8b4f689f89368683fe4171516d64
clay <clay@cdd732f4-07f7-4c14-856a-aa9830f7e343>
2011-10-08 00:25:16 +0000

    git-svn-id: http://corp.ratecred.com/svn/sightly-ratecred-service/trunk@2171 cdd732f4-07f7-4c14-856a-aa9830f7e343

--- etl/093/update_20111006.sql
+++ etl/093/update_20111006.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `cust_order` ADD COLUMN `publisher_id` BIGINT(20) NULL  AFTER `patron_id` ;
-
+ALTER TABLE `publisher` CHANGE COLUMN  `simple_geo_json_token` `json_token` VARCHAR(255) NULL DEFAULT NULL  ;
